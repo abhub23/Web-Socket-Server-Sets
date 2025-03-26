4ef04e1bfa3cc5d2d170c2d418881b9a5369bd83
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-03-26 12:34:37 +0530

    refactor

--- Typescript/index.ts
+++ Typescript/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import http from 'http'
-import { Server } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 
 const app = express();
 const httpserver = http.createServer(app)
@@ -10,4 +10,6 @@ const io = new Server(httpserver, {
     }
 })
 
-
+io.on('connection', (socket: Socket) => {
+    io.emit(`New user connected ${socket.id}`)
+})
