0f8762c2076e1f40e598687e6929f04c102fa147
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-05-03 03:57:18 +0530

    feat: fonts added

---
+++ next-socket-client/utils/fonts.ts
@@ -0,0 +1,8 @@
+import { Bricolage_Grotesque } from "next/font/google";
+
+
+const Bricolage_font = Bricolage_Grotesque({
+    subsets: ['latin'],
+})
+
+export const Bricolage = Bricolage_font.className
