78398ec62dd4b051926a386746db093c51872dec
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-05-07 22:50:08 +0530

    fix: to fix the hydration error about the changing of svg of moon and sun because of themes, i created a Saferender component to safely render it without the hydration error

---
+++ next-socket-client/utils/Saferender.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+type Child = {
+    children: React.ReactNode
+}
+
+export const SafeRender:React.FC<Child> = ({children}) => {
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) return null // or return a placeholder/spinner
+
+  return <>{children}</>
+}
