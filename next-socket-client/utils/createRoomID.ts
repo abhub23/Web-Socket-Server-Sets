85d539b136b47a53609e0b3763cb81b89f8238a9
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-05-08 23:44:40 +0530

    fix: returns string instead of a number

--- next-socket-client/utils/createRoomID.ts
+++ next-socket-client/utils/createRoomID.ts
@@ -1,4 +1,4 @@
 export const createRoomID = () => {
-    const Id = Math.floor(1000 + Math.random() * 9000)
-    return Id
-}
+  const Id = Math.floor(1000 + Math.random() * 9000);
+  return String(Id);
+};
