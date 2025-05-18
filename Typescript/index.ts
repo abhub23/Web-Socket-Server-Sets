ce69b6f89639879674b60c73a8c1544cf083f7ec
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-05-18 23:17:02 +0530

    basic server init

--- Typescript/index.ts
+++ Typescript/index.ts
@@ -3,13 +3,30 @@ import http from 'http'
 import { Server, Socket } from 'socket.io';
 
 const app = express();
+const Port = 6006
 const httpserver = http.createServer(app)
 const io = new Server(httpserver, {
     cors: {
-        origin: '*'
+        origin: 'http://localhost:3000'
     }
 })
 
 io.on('connection', (socket: Socket) => {
-    io.emit(`New user connected ${socket.id}`)
+    console.log(`Client connected with socket ID: ${socket.id}`)
+
+    socket.on('private-chat', (roomId: string, username: string) => {
+        socket.join(roomId)
+
+    })
+
+    socket.on('message', (roomId: string, chatmessage: string) => {
+        io.to(roomId).emit('receive-message', ({
+            senderId: socket.id,
+            message: chatmessage
+        }))
+    })
+})
+
+httpserver.listen(Port, () => {
+    console.log(`SocketIO Server Listening on Port: ${Port}`)
 })
