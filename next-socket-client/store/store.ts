2a27c2da163d2648a423e0084a823f1e317246da
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-05-20 03:37:17 +0530

    added time in message object for real time

--- next-socket-client/store/store.ts
+++ next-socket-client/store/store.ts
@@ -1,48 +1,50 @@
-import {create} from 'zustand'
+import { create } from 'zustand';
 
 type Roomtype = {
-    roomId: string;
-    setRoomId: (state: string) => void
-}
+  roomId: string;
+  setRoomId: (state: string) => void;
+};
 
 export const useRoomId = create<Roomtype>((set) => ({
-    roomId: '',
-    setRoomId: (state: string) => set({roomId: state})
-}))
+  roomId: '',
+  setRoomId: (state: string) => set({ roomId: state }),
+}));
 
 type Usernametype = {
-    username: string;
-    setUsername: (state: string) => void
-}
+  username: string;
+  setUsername: (state: string) => void;
+};
 
 export const useUsername = create<Usernametype>((set) => ({
-    username: '',
-    setUsername: (state: string) => set({username: state})
-}))
+  username: '',
+  setUsername: (state: string) => set({ username: state }),
+}));
 
 type Generatetype = {
-    generate: boolean;
-    setGenerate: (state: boolean) => void
-}
+  generate: boolean;
+  setGenerate: (state: boolean) => void;
+};
 
 export const useGenerate = create<Generatetype>((set) => ({
-    generate: false,
-    setGenerate: (state: boolean) => set({generate: state})
-}))
+  generate: false,
+  setGenerate: (state: boolean) => set({ generate: state }),
+}));
 
-type Msgtype = {
-  senderId : string;
-  message: string
-}
+export type Msgtype = {
+  senderId: string;
+  message: string;
+  time: string;
+};
 
 type Messagetype = {
-    message: Array<Msgtype>;
-    addMessage: (newMessage: Array<Msgtype>) => void
-}
+  message: Array<Msgtype>;
+  addMessage: (newMessage: Msgtype) => void;
+};
 
 export const useMessage = create<Messagetype>((set) => ({
-    message: [],
-    addMessage: (newMessage: Array<Msgtype>)  => set((state) => ({
-        message: [...state.message, ...newMessage]
-    }))
-}))
+  message: [],
+  addMessage: (newMessage: Msgtype) =>
+    set((state) => ({
+      message: [...state.message, newMessage],
+    })),
+}));
