fd92a99ffd02f1923ee2b37e43233b52c37e04af
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-05-22 22:36:48 +0530

    client and server types added

--- next-socket-client/utils/socket.ts
+++ next-socket-client/utils/socket.ts
@@ -1,10 +1,40 @@
 // lib/socket.ts
-import { io } from 'socket.io-client';
+import { io, Socket } from 'socket.io-client';
 
-const socket = io(process.env.NEXT_PUBLIC_BACKEND_SOCKET_URL!, {
+//Events Emitted by Client -> Server
+type ClienttoServerEvents = {
+  'private-chat': (roomId: string, username: string) => void;
+  'create-room': () => void;
+  'message' :  (roomId: string, chatmessage: string, time: string) => void;
+}
+
+
+type Msgtype = {
+  senderId: string;
+  message: string;
+  time: string;
+};
+
+//Events Emitted by Server -> Client
+type ServertoClientEvents = {
+  'room-created': (roomId: string) => void; 
+  'receive-message': (data : Msgtype) => void;
+  'socket-length': (data : number) => void;
+}
+
+const socket: Socket<ServertoClientEvents, ClienttoServerEvents> = io(process.env.NEXT_PUBLIC_BACKEND_SOCKET_URL || 'http://0.0.0.0:6006', {
   autoConnect: false,
 });
 
 export default socket;
 
-//to maintail single socket across landing page where actual socket.connect() is happening
+//to maintain single socket connection across landing page where actual socket.connect() is happening
+
+
+/*
+Server generic order: <ClientToServerEvents, ServerToClientEvents>
+
+Socket generic order: <ServerToClientEvents, ClientToServerEvents>
+
+They are opposite because the point of view is different (server vs client).
+*/
