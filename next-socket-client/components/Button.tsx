95f7186e2f4f247294c8afca685e490ba1f4017c
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-05-23 23:57:41 +0530

    fix ui

--- next-socket-client/components/Button.tsx
+++ next-socket-client/components/Button.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 type clickType = {
-    onClick: () => void
+  onClick: () => void
 }
 
 const Button: React.FC<clickType> = ({onClick}) => {
@@ -11,7 +11,7 @@ const Button: React.FC<clickType> = ({onClick}) => {
       <button onClick={onClick}>
         <div className="svg-wrapper-1">
           <div className="svg-wrapper">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={24} height={24}>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className='lg:w-[24px] lg:h-[24px] h-[16px] w-[16p]]'>
               <path fill="none" d="M0 0h24v24H0z" />
               <path fill="currentColor" d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z" />
             </svg>
@@ -27,11 +27,11 @@ const Button: React.FC<clickType> = ({onClick}) => {
 const StyledWrapper = styled.div`
   button {
     font-family: inherit;
-    font-size: 18px;
+    font-size: 14px;
     background: royalblue;
     color: white;
-    padding: 0.4em 1em;
-    padding-left: 0.9em;
+    padding: 0.3em 0.8em;
+    padding-left: 0.8em;
     display: flex;
     align-items: center;
     border: none;
@@ -77,6 +77,17 @@ const StyledWrapper = styled.div`
     to {
       transform: translateY(-0.1em);
     }
-  }`;
+  }
+
+  /* Tailwind's sm: breakpoint = 640px */
+  @media (min-width: 640px) {
+    button {
+      font-size: 18px;
+      padding: 0.4em 1em;
+      padding-left: 0.9em;
+    }
+  }
+`
+
 
 export default Button;
