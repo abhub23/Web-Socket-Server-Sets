7189b2bfeecf6ea9668fe517ca667c9faa73df1c
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-05-27 23:49:07 +0530

    some minor changes

--- Typescript/index.ts
+++ Typescript/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import http from 'http'
 import { Server, Socket } from 'socket.io';
+import { createRoomID } from './createRoomID';
 
 const app = express();
 const Port = 6006
@@ -14,19 +15,38 @@ const io = new Server(httpserver, {
 io.on('connection', (socket: Socket) => {
     console.log(`Client connected with socket ID: ${socket.id}`)
 
-    socket.on('private-chat', (roomId: string, username: string) => {
+
+    socket.on('create-room', () => {
+        const roomId = createRoomID()
+        socket.emit('room-created', roomId)
+    })
+
+
+    socket.on('private-chat', async (roomId: string, username: string) => {
         socket.join(roomId)
+
+        const roomSockets = await io.in(roomId).fetchSockets();
+        const length = roomSockets.length;
+        
+        setTimeout(() => {
+            io.to(roomId).emit('socket-length', length);
+        }, 1000);
+
 
     })
 
     socket.on('message', (roomId: string, chatmessage: string, time: string) => {
-        console.log(roomId, chatmessage,time)
+        console.log(roomId, chatmessage, time)
+
         io.to(roomId).emit('receive-message', ({
             senderId: socket.id,
             message: chatmessage,
             time: time
         }))
+
     })
+
+
 
     socket.on('disconnect', () => {
         console.log(`Client ${socket.id} Disconnected!`)
@@ -34,6 +54,6 @@ io.on('connection', (socket: Socket) => {
 
 })
 
-httpserver.listen(Port,'0.0.0.0', () => {
+httpserver.listen(Port, '0.0.0.0', () => {
     console.log(`SocketIO Server Listening on Port: ${Port}`)
 })
