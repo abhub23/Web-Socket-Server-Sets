45f02f52c0a354d412531618ca929fb40da87418
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-06-05 15:01:05 +0530

    minor change

--- next-socket-client/components/Footer.tsx
+++ next-socket-client/components/Footer.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 const Footer = () => {
   return (
-    <div className="mt-[152px] flex lg:h-6 h-6 items-center justify-center text-[12px] lg:mt-[150px] lg:text-[16px] dark:text-white">
+    <div className="mt-[152px] flex lg:h-6 h-6 items-center justify-center text-[12px] lg:mt-[142px] lg:text-[16px] dark:text-white">
       Designed and Developed by
       <Link
         className="ml-1 text-transparent"
