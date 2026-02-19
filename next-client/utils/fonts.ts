2c18764fb8fd1954f428e43a03061e8fda964152
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2026-02-20 00:18:53 +0530

    removed unnecessary font

--- next-client/utils/fonts.ts
+++ next-client/utils/fonts.ts
@@ -1,17 +1,8 @@
-import {
-  Bricolage_Grotesque,
-  IBM_Plex_Mono,
-  JetBrains_Mono,
-} from 'next/font/google';
-
-const Bricolage_font = Bricolage_Grotesque({
-  subsets: ['latin'],
-});
+import { JetBrains_Mono } from 'next/font/google';
 
 const JetMono_font = JetBrains_Mono({
   weight: '400',
   subsets: ['latin'],
 });
 
-export const Bricolage = Bricolage_font.className;
-export const Jetmono = JetMono_font.className;
+export const jetmono = JetMono_font.className;
